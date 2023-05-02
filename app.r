43fd9170ca589dbb5634d0023e13f17ac6a2067d
Adam Lear <adamlear@Adams-MacBook-Air.local>
2023-05-02 09:50:36 -0500

    first 2 steps of cleaning

--- app.r
+++ app.r
@@ -1,8 +1,15 @@
 data <- read.csv("Airbnb_Open_Data.csv")
 
+dim(data)
+names(data)
+
 # remove irrelevant columns
-data <- data[]
+data <- data[, -c(2, 3, 5, 7, 8, 9, 10, 11, 20, 25, 26)]
+names(data)
+
+# remove null rows
+sum(is.na(data))
+data <- na.omit(data)
+sum(is.na(data))
 
 attach(data)
-
-
