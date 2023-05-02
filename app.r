5312b4ff63861594a20545136f278d1d2fc6cd96
PossiblyAPanda <78812701+PossiblyAPanda@users.noreply.github.com>
2023-05-02 09:10:14 -0500

    test
    test

--- app.r
+++ app.r
@@ -3,3 +3,9 @@ data <- read.csv("Airbnb_Open_Data.csv")
 # remove irrelevant columns
 data <- data[]
 
+attach(data)
+
+glm.fit <- glm(price ~ id + host_identity_verified + neighbourhood.group + instant_bookable + cancellation_policy
+               + room.type + Construction.year + service.fee + minimum.nights + number.of.reviews + reviews.per.month
+               + review.rate.number + calculated.host.listings.count + availability.365, 
+               data = data, family = binomial)
