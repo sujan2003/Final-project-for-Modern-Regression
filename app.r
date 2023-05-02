8444492fdb109e2aa033000568d67977b320cd8b
PossiblyAPanda <78812701+PossiblyAPanda@users.noreply.github.com>
2023-05-02 09:11:24 -0500

    test revert
    revert

--- app.r
+++ app.r
@@ -5,7 +5,4 @@ data <- data[]
 
 attach(data)
 
-glm.fit <- glm(price ~ id + host_identity_verified + neighbourhood.group + instant_bookable + cancellation_policy
-               + room.type + Construction.year + service.fee + minimum.nights + number.of.reviews + reviews.per.month
-               + review.rate.number + calculated.host.listings.count + availability.365, 
-               data = data, family = binomial)
+
