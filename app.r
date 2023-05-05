a8e5be9a2f2e7636e46f49edf343e513447d1839
Adam Lear <adamlear@Adams-MacBook-Air.local>
2023-05-05 16:35:36 -0500

    finish data cleaning and fit MLR

--- app.r
+++ app.r
@@ -1,24 +1,47 @@
 # install.packages("fastDummies")
 library(fastDummies)
 
-data <- read.csv("data/Airbnb_Open_Data.csv")
+data <- read.csv("Final-project-for-Modern-Regression/Airbnb_Open_Data.csv")
 
 dim(data)
 names(data)
 
-# remove null rows
-sum(is.na(data))
-data <- na.omit(data)
-sum(is.na(data))
 
+# DATA CLEANING ###############################################################
 # create dummy variables
 data <- dummy_cols(data,
                    select_columns = c("host_identity_verified", 
                                       "neighbourhood.group",
                                       "cancellation_policy",
                                       "room.type"))
+dim(data)
 
 # remove irrelevant and repeat columns
-data <- data[, -c(2:14, 20, 25:27)]
+data <- data[, -c(1:14, 20, 25:27, 34, 38)]
 dim(data)
-names(data)                   
+head(data) 
+
+# remove dollar signs from data$price and data$service.fee
+data[] <- lapply(data, gsub, pattern="$", fixed=TRUE, replacement="")
+head(data)
+
+# convert character types to numeric
+char_to_num <- c(2:9)
+
+for (i in char_to_num) {
+  data[ , i] <- as.numeric(data[ , i])
+}
+
+# remove rows with null values
+sum(is.na(data))
+data <- na.omit(data)
+sum(is.na(data))
+
+attach(data)
+
+
+# RUN MODELS ##################################################################
+
+# Testing to run MLR
+mlr.fit <- lm(price ~ .,
+              data = data)
